ea381feb34b46c49fc80eb1cdd4c80b772633244
Cameron Cundiff <ckundo@gmail.com>
2011-09-06 15:54:41 -0400

    mailer text changes

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,15 +1,15 @@
 class Mailer < ActionMailer::Base
   default_url_options[:host] = DEFAULT_HOST
-  default :from => "Workflow <info@getworkflow.com>"
+  default :from => "Kate Singleton <kate@arthound.net>"
   
   
   def confirmation(person)
     @referral_code = person.referral_code
-    mail(:to => person.email, :subject => "You've been added to our beta invite list")
+    mail(:to => person.email, :subject => "Thanks for signing up for Buy Some Damn Art!")
   end
   
   def vip(person)
     @referral_code = person.referral_code
-    mail(:to => person.email, :subject => "Congratulations! You're on our VIP list")  
+    mail(:to => person.email, :subject => "Thanks for the referral!")
   end  
 end
--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
-DEFAULT_HOST = 'getworkflow.com'
+DEFAULT_HOST = 'buysomedamnart.com'
 
 module LaunchingSoon
   class Application < Rails::Application
