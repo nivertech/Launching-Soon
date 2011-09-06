d7df4a6c5b8fe6eb76d0cd2bf4e18d82d7b4fa6d
Cameron Cundiff <ckundo@gmail.com>
2011-09-06 16:15:07 -0400

    tweaked template text

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 LaunchingSoon::Application.routes.draw do
   resources :people
   match ':referral_code' => 'people#new', :as => :referral  
-  root :to => 'people#new'
+  root :to => 'people#new', :as => '/'
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
