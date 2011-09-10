04a72ce22d2e35e9d7befc81ca9d383c0620aa39
Cameron Cundiff <ckundo@gmail.com>
2011-09-10 18:06:11 -0400

    adjusted form element attributes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :no_www
-  
-  private
-  
-  def no_www
-    if /^www/.match(request.host)
-      redirect_to 'http://' + DEFAULT_HOST + request.request_uri
-      flash.keep
-    end
-  end
 end
