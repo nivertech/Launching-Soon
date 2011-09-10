9739b30ba14fd64fbe13a40bd892e473aad84bff
Cameron Cundiff <ckundo@gmail.com>
2011-09-10 17:21:33 -0400

    adjusted font color for notice and changed from address in email

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,11 +1,11 @@
 class Mailer < ActionMailer::Base
   default_url_options[:host] = DEFAULT_HOST
-  default :from => "Kate Singleton <kate@arthound.net>"
+  default :from => "Buy Some Damn Art <buysomedamnart@arthound.net>"
   
   
   def confirmation(person)
     @referral_code = person.referral_code
-    mail(:to => person.email, :subject => "Thanks for signing up for Buy Some Damn Art!")
+    mail(:to => person.email, :subject => "Thanks for signing up!")
   end
   
   def vip(person)
